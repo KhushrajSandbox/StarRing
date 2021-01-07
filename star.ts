bfcec7f745567d9c73ad40997b287c215b98ef71
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-07 10:04:23 +0530

    Clean more, don't mix await & .then, fail nicely

--- star.ts
+++ star.ts
@@ -1,15 +1,22 @@
-const users: string[] = await(fetch('https://raw.githubusercontent.com/KhushrajRathod/StarRing/main/users.json')).then(r => {r.json()});
+const usersResponse = await fetch('https://raw.githubusercontent.com/KhushrajRathod/StarRing/main/users.json')
+const users: string[] = await usersResponse.json()
+
 users.forEach(async user => {
-    const userRepos = await(fetch(`https://api.github.com/users/${user}/repos`)).then(r => {r.json()});
-    userRepos.forEach((userRepo: any) => {
-        fetch(`https://api.github.com/user/starred/${user}/${userRepo.name}`, {
-            method: 'PUT',
-            headers: {
-                'Authorization': `token ${Deno.args[0]}`,
-                'Content-Length': "0",
-                'Accept': "application/vnd.github.v3+json"
-            }
+    try {
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos`)
+        const userRepos = await reposResponse.json()
+
+        userRepos.forEach((userRepo: any) => {
+            fetch(`https://api.github.com/user/starred/${user}/${userRepo.name}`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `token ${Deno.args[0]}`,
+                    'Content-Length': "0",
+                    'Accept': "application/vnd.github.v3+json"
+                }
+            })
         })
-    })
+    } catch {}
 })
+
 export { } // Classify as TypeScript module
